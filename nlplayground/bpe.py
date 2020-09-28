58d63e458f9dde0416cefe9eafeea5112c1a14b7
FrankHellweg <FrankHellweg@gmx.net>
2020-09-28 22:20:11 +0200

    Basic stubs for BPE and Vocabulary added.

---
+++ nlplayground/bpe.py
@@ -0,0 +1,13 @@
+
+
+class Vocabulary:
+    def __init__(self):
+        self.voc = set()
+
+
+def createVocabularyBPE(text):
+    voc = Vocabulary()
+    return voc
+
+if __name__ == "__main__":
+    print("Hello World")
